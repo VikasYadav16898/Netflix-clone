b74bf76e51195a5f6d49f29a05e828b0a9ba98cb
Yaduveera <vikasyashyadav@gmail.com>
2021-01-17 23:56:45 +0530

    complete v2

--- src/api.js
+++ src/api.js
@@ -4,4 +4,3 @@ export const API_KEY = process.env.REACT_APP_API_KEY;
 export const movieTrendList = base_url + "trending/all/week" + API_KEY;
 export const genres = base_url + "genre/movie/list" + API_KEY;
 export const imageURL = "https://image.tmdb.org/t/p/w500/";
-// https://image.tmdb.org/t/p/w500/9v43jAAfCYcQEgeMJ1H0rghN0of.jpg
